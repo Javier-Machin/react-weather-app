a08438a89ea5e710701426ceda92143a2a4c6d3e
Javier Machin <javier_machin_acosta@hotmail.com>
2018-10-10 19:56:17 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+
+class WeatherDataDisplay extends React.Component {
+  render() {
+    const data = this.props.data;
+    const code = parseInt(this.props.data.cod);
+    
+    switch (code) {
+      case 200:
+        return(
+          <React.Fragment>
+          <h1>
+            {data.name} temperature: {Math.round(data.main.temp - 273.15)}ºC
+          </h1>
+          <p>{data.clouds.all > 50 ? "cloudy" : "clear"}</p>
+          </React.Fragment>
+        );
+      case 404:
+        return(<h1>City not found</h1>);
+      case 503:
+        return(<h1>Service unavailable</h1>);
+      default:
+        return null;
+      }
+  };
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      data: {cod: 0}
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="main-container">
+        <WeatherDataDisplay data={this.state.data} />
       </div>
     );
   }
+
+  async componentDidMount() {
+    try {
+      const response = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=provincia+de+las+palmas&APPID=a58c376530b8af49dfeb836d445fd911`
+      );
+      const json = await response.json();
+      console.log(json);
+      this.setState({ data: json });
+    } catch(error) {
+      this.setState({ data: {cod: 503}});
+    }
+  }
+
 }
 
 export default App;
