3ff47f9ae298e304ead651959572d9aa1816c48e
Javier Machin <javier_machin_acosta@hotmail.com>
2018-10-10 19:57:22 +0100

    Updates

---
+++ precache-manifest.02b019f064899c24f9eee3c75ab0d839.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "72a6b7ef5541b624727a",
+    "url": "/react-weather-app/static/js/runtime~main.72a6b7ef.js"
+  },
+  {
+    "revision": "0840869bf396436e3892",
+    "url": "/react-weather-app/static/js/main.0840869b.chunk.js"
+  },
+  {
+    "revision": "44b89ba006958676ca66",
+    "url": "/react-weather-app/static/js/1.44b89ba0.chunk.js"
+  },
+  {
+    "revision": "0840869bf396436e3892",
+    "url": "/react-weather-app/static/css/main.a80e6fb2.chunk.css"
+  },
+  {
+    "revision": "22c5ea4b31110229a42b843fd501edd8",
+    "url": "/react-weather-app/index.html"
+  }
+];
