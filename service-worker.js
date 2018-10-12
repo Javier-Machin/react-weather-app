288f45709da23514ea2e5dc2a285f6ec9f50b9e8
Javier Machin <javier_machin_acosta@hotmail.com>
2018-10-12 22:22:03 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/react-weather-app/precache-manifest.17640412ab79dc4ec63904d74dff0826.js"
+  "/react-weather-app/precache-manifest.a40abaaabd8fdad375340dbd931542ae.js"
 );
 
 workbox.clientsClaim();
