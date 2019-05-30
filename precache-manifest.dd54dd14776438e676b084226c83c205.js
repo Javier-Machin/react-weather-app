f973624ea7c877df7030e71dcb9d516fa77eb872
Javi M <javier_machin_acosta@hotmail.com>
2019-05-30 15:14:44 +0100

    Updates

---
+++ precache-manifest.dd54dd14776438e676b084226c83c205.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87573278a5a7e086a5af61811b21caf1",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "ee1144d371d515aca144",
+    "url": "/react-weather-app/static/css/main.7f269bfa.chunk.css"
+  },
+  {
+    "revision": "a0bb8ef5f9e6fd383a2d",
+    "url": "/react-weather-app/static/js/2.0f6d48c1.chunk.js"
+  },
+  {
+    "revision": "ee1144d371d515aca144",
+    "url": "/react-weather-app/static/js/main.5861f88a.chunk.js"
+  },
+  {
+    "revision": "e736fc5812ad96aa92b3",
+    "url": "/react-weather-app/static/js/runtime~main.7e96bc80.js"
+  },
+  {
+    "revision": "e0bb6c51b726eb400a9dfea4ad6895f8",
+    "url": "/react-weather-app/static/media/rain.e0bb6c51.png"
+  },
+  {
+    "revision": "40382ff207985cb639784be61beac0a0",
+    "url": "/react-weather-app/static/media/weather-city.40382ff2.jpg"
+  }
+]);
